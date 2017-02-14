d89eb2e537bb917e896fc07511db25ac495c9c04
Jim-Holmstroem <jim.holmstroem@gmail.com>
2017-02-14 22:38:40 +0000

    Everything surrounding the stack machine without the actual stack machine

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,6 +4,8 @@ module Lib
 
 import           Prelude hiding (Word)
 
+import           Data.Maybe
+
 import           Data.Bits
 
 import           Data.Word
@@ -29,11 +31,11 @@ unaryApply BitComplement = complement
 unaryApply (BitsRotate n) = flip rotate n
 
 data BinaryOperation w = Addition
-                       | Subtraction
-                       | Multiplication
-                       | BitOr
-                       | BitAnd
-                       | BitXor
+                      | Subtraction
+                      | Multiplication
+                      | BitOr
+                      | BitAnd
+                      | BitXor
     deriving (Show, Eq)
 binaryApply :: (Bits w, Num w) => BinaryOperation w -> w -> w -> w
 binaryApply Addition = (+)
@@ -44,10 +46,76 @@ binaryApply BitAnd = (.&.)
 binaryApply BitXor = xor
 
 
-type Word = Word32
+
 data Instruction = NoOp
+                | Store
+                | Retrieve
+
+                | Binary (BinaryOperation WordUnit)
+                | Unary (UnaryOperation WordUnit)
+
+                | Drop
+                | Duplicate
+                | Swap
+                | Over
+
+                | PushReturn
+                | PopReturn
+
+                | Call
+                | Exit
+
+                | If
+
+                | Constant WordUnit
     deriving (Show, Eq)
 
 
+type Program = [Instruction]
+type PC = WordUnit
+type Input = [WordUnit]
+type Output = WordUnit
+type Memory = [WordUnit]
+type DataStack = Stack WordUnit
+type ReturnStack = Stack WordUnit
+
+data Machine = Machine { pc :: WordUnit  -- TODO pc :: PC (via type PC = WordUnit)
+                       , program  :: Program
+                       , input :: Input
+                       , memory :: Memory
+                       , dataStack :: DataStack
+                       , returnStack :: ReturnStack
+                       }
+
+machineStart program input = Machine { pc = 0
+                                     , program = program
+                                     , input = input
+                                     , memory = take (2^16) (repeat 0)  -- TODO hard to modify..
+                                     , dataStack = Stack.empty
+                                     , returnStack = Stack.empty
+                                     }
+
+step :: Machine -> (Machine, Maybe Output)
+step machine = performInstruction (decodeInstruction (program machine) (pc machine)) machine
+    where decodeInstruction program pc = program !! (fromIntegral pc)
+          performInstruction NoOp machine = (machine, Nothing)
+
+output :: Program -> Input -> [Output]
+output program input = catMaybes $ output_ $ machineStart program input
+    where output_ machine
+              | isDone machine = []
+              | otherwise = step_output : output_ machine'
+                  where isDone (Machine { program = program , pc = pc}) = length program > fromIntegral pc
+                        (machine', step_output) = step machine
+
+run :: Program -> Input -> IO ()
+run program input = do
+    mapM_ print $ output program input
+    putStrLn "exit"
+
+
 render :: IO ()
-render = print $ unaryApply BitComplement (2 :: WordUnit)
+--render = print $ unaryApply BitComplement (2 :: WordUnit)
+render = run [ NoOp
+             , NoOp
+             ] [1..]
